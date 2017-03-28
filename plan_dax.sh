73fe41ae5e5b1f834219c59419783d709e4e7613
Rafael Ferreira da Silva <rafsilva@isi.edu>
2017-03-28 10:24:11 -0700

    generalizing replica catalog

--- plan_dax.sh
+++ plan_dax.sh
@@ -7,6 +7,13 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
+cat > rc.txt << EOF
+hadoop.tgz http://apache.parentingamerica.com/hadoop/common/hadoop-2.7.3/hadoop-2.7.3.tar.gz site="local"
+spark.tgz http://d3kbcqa49mib13.cloudfront.net/spark-2.1.0-bin-hadoop2.7.tgz site="local"
+hosts.txt file:///$DIR/input/hosts.txt site="local"
+sample.txt file:///$DIR/input/sample.txt site="local"
+EOF
+
 DAXFILE=$1
 
 # This command tells Pegasus to plan the workflow contained in 
